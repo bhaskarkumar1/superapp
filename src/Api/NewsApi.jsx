9d2a1c14834d79ffd47746e3f7b8390871225ee3
Bhaskar <bhaskarkumar.india@gmail.com>
2024-03-10 03:55:48 +0530

    fixing for production on netlify

--- src/Api/NewsApi.jsx
+++ src/Api/NewsApi.jsx
@@ -2,7 +2,7 @@
 const NewsApi= async()=>{
     // const API_KEY="b5ac8c5c578d436faf5b7034d6ade57f"
 
-    const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.REACT_APP_NEWS_API}`)
+    const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API}`)
 
     const resJson= await res.json()
 
