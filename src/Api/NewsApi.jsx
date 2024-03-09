05466d3cd6e901474b7f8e83800829e324c9ae1a
Bhaskar <bhaskarkumar.india@gmail.com>
2024-03-10 04:13:35 +0530

    minor changes for deployment-trial-5!

--- src/Api/NewsApi.jsx
+++ src/Api/NewsApi.jsx
@@ -1,6 +1,6 @@
 
 const NewsApi= async()=>{
-    // const API_KEY="b5ac8c5c578d436faf5b7034d6ade57f"
+  
 
     const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API}`)
 
--- src/Api/WeatherApi.jsx
+++ src/Api/WeatherApi.jsx
@@ -1,4 +1,4 @@
-// const API="d66aa2315ba0e92adf6fae8afbc4ad84"
+
 const city="hyderabad"
 const WeatherApi=async ()=>{
     let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_API}`
