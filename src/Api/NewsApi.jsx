7dcfe1257d4df68d6fa25762a0d86cfc8e1e966c
Bhaskar <bhaskarkumar.india@gmail.com>
2024-03-10 03:34:50 +0530

    ready-2-deploy

--- src/Api/NewsApi.jsx
+++ src/Api/NewsApi.jsx
@@ -1,8 +1,8 @@
 
 const NewsApi= async()=>{
-    const API_KEY="b5ac8c5c578d436faf5b7034d6ade57f"
+    // const API_KEY="b5ac8c5c578d436faf5b7034d6ade57f"
 
-    const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.REACT_APP_NEWS_API}`)
 
     const resJson= await res.json()
 
--- src/Api/WeatherApi.jsx
+++ src/Api/WeatherApi.jsx
@@ -1,7 +1,7 @@
-const API="d66aa2315ba0e92adf6fae8afbc4ad84"
+// const API="d66aa2315ba0e92adf6fae8afbc4ad84"
 const city="hyderabad"
 const WeatherApi=async ()=>{
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API}`
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_REACT_APP_WEATHER_API}`
 
     const data= await fetch(url)
     const res=await data.json();
