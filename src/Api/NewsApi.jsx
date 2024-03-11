5ce480be43cdbda66ee645b7843927f465de4044
Bhaskar <bhaskarkumar.india@gmail.com>
2024-03-11 20:47:43 +0530

    trying to fix news api prevs prblm

--- src/Api/NewsApi.jsx
+++ src/Api/NewsApi.jsx
@@ -3,7 +3,9 @@ const NewsApi= async()=>{
   
 
     // const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API}`)
-    const res= await fetch(`https://api.worldnewsapi.com/search-news?text=chatgpt&language=en&api-key=${import.meta.env.VITE_NEWS_API}`)
+    const res= await fetch(` https://api.worldnewsapi.com/search-news?api-key=${import.meta.env.VITE_NEWS_API}&text=chatgpt`)
+   
+
     const resJson= await res.json()
 
     return resJson.news[3]
